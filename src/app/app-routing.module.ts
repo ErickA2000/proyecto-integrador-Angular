ee13e9258e16f8abd70a5d225e31756bbb19f348
ErickA2000 <erickwladimirarevalonavarro@gmail.com>
2021-11-13 12:03:19 -0500

    primera subida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './componentes/about/about.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { ProductosComponent } from './componentes/productos/productos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent},
+  { path: 'productos', component: ProductosComponent},
+  { path: 'about', component: AboutComponent},
+  { path: '**', pathMatch: 'full', redirectTo: 'inicio'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { ProductosComponent } from './componentes/productos/productos.component';
+import { AboutComponent } from './componentes/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    InicioComponent,
+    ProductosComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
